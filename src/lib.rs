eee9ed9c8bc738cf6502e88e99539c58a6a81266
Brad Gibson <u007d@humanenginuity.com>
2023-06-06 16:20:21 -0700

    make entire `Error` hierarchy public

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 #![feature(const_option, const_type_name, stmt_expr_attributes)]
 
-mod error;
+pub mod error;
 mod hex_value;
 mod shared_consts;
 
-pub use error::{Error, Result};
+pub use error::*;
 
 pub use crate::hex_value::HexValue;
